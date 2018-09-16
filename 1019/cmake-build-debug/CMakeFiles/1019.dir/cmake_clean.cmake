2ea48e59cf3a377843aa86b940e271481f387edb
Mazedul1737820124 <mazedulislam4970@gmail.com>
2018-09-17 01:16:11 +0600

    Add files via upload

---
+++ 1019/cmake-build-debug/CMakeFiles/1019.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1019.dir/main.c.o"
+  "1019.pdb"
+  "1019"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/1019.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
